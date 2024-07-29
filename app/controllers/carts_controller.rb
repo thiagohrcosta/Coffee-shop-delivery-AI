d978ece245b9728c6d88942638715c06619a62e0
thiagohrcosta <thiagohrcosta86@gmail.com>
2024-07-29 18:07:32 -0300

    Complete add products to cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,6 @@
 class CartsController < ApplicationController
   before_action :user_is_logged?
+  before_action :set_cart, only: [:show]
 
   def show;end
 
@@ -8,18 +9,21 @@ class CartsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
 
-    return if user_has_active_cart?
-
-    ActiveRecord::Base.transaction do
-      @cart = Cart.new(user_id: current_user.id)
-      @cart.user_id = current_user.id
-      binding.pry
-
-      if @cart.save
-        @cart_product = CartProduct.create!(cart_id: @cart.id, product_id: @product.id)
-        redirect_to cart_path(@cart.id), notice: 'Product added to cart.'
-      else
-        raise ActiveRecord::Rollback
+    if user_has_active_cart?
+      @cart = Cart.find_by(user_id: current_user.id)
+      @cart_product = CartProduct.create!(cart_id: @cart.id, product_id: @product.id)
+      redirect_to cart_path(@cart.id)
+    else
+      ActiveRecord::Base.transaction do
+        @cart = Cart.new(user_id: current_user.id)
+        @cart.user_id = current_user.id
+  
+        if @cart.save
+          @cart_product = CartProduct.create!(cart_id: @cart.id, product_id: @product.id)
+          redirect_to cart_path(@cart.id), notice: 'Product added to cart.'
+        else
+          raise ActiveRecord::Rollback
+        end
       end
     end
   rescue ActiveRecord::RecordInvalid => e
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   def home
     @products = Product.all
-    @cart = Cart.find_by(user_id: current_user.id)
+    if current_user.present? && current_user.access == "user"
+      @cart = Cart.find_by(user_id: current_user.id)
+    end 
   end
 end
